7f30a018c84da5a13072ec86a8b36acd358d282a
August Estidola <aie.estidola@gmail.com>
2023-11-06 19:09:30 +0800

    added client controller and home#index

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,10 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root 'welcome#index'
+
+  namespace :client do
+    root 'home#index'
+  end
+
+
 end
