b5b10c1bc0c59329c81de2f01d110e8bb390622e
August Estidola <aie.estidola@gmail.com>
2023-11-06 19:54:27 +0800

    fixed routes and removed header

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,23 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, as: :client, path: 'client', controllers: {
+    sessions: 'client/users/sessions'
+  }
+
+  devise_for :users, as: :admin, path: 'admin', controllers: {
+    sessions: 'admin/users/sessions'
+  }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
   root 'welcome#index'
 
-  namespace :client do
-    root 'home#index'
+  namespace :admin do
+    root "home#index"
   end
 
-  namespace :admin do
-    root 'home#index'
+  namespace :client do
+    root "home#index"
   end
+
 end
