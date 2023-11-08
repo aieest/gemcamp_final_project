1685b6131ce32a738cfa4e65968469946466b663
August Estidola <aie.estidola@gmail.com>
2023-11-08 14:15:20 +0800

    FEATURE 1(EDIT) - Fiex admin/home_controller.rb

--- app/controllers/admin/home_controller.rb
+++ app/controllers/admin/home_controller.rb
@@ -1,9 +1,6 @@
 class Admin::HomeController < ApplicationController
   before_action :authenticate_admin_user!
-  before_action :check_admin
   def index; end
 
-  def check_admin
-    raise ActionController::RoutingError.new('Not Found') unless current_user.admin?
-  end
+
 end
