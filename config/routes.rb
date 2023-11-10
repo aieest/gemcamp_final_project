6551c35e315a61f9de8ce97cfe7fdc04c5e63c4e
August Estidola <aie.estidola@gmail.com>
2023-11-10 18:13:52 +0800

    FEATURE 6 - Added serializers for region, province, city and barangay

--- config/routes.rb
+++ config/routes.rb
@@ -22,4 +22,21 @@ Rails.application.routes.draw do
     }
   end
 
+  namespace :api do
+    namespace :v1 do
+      resources :regions, only: [:index, :show], defaults: { format: :json } do
+        resources :provinces, only: :index, defaults: { format: :json }
+      end
+
+      resources :provinces, only: [:index, :show], defaults: { format: :json } do
+        resources :cities, only: :index, defaults: { format: :json }
+      end
+
+      resources :cities, only: [:index, :show], defaults: { format: :json } do
+        resources :barangays, only: :index, defaults: { format: :json }
+      end
+
+      resources :barangays, only: [:index, :show], defaults: { format: :json }
+    end
+  end
 end
