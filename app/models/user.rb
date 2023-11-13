c57a4fe1173bb27b42582f1e29597eb019cc9a2b
August Estidola <aie.estidola@gmail.com>
2023-11-13 15:45:07 +0800

    FEATURE 8 - Added counter cache and reset counters

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,8 @@ class User < ApplicationRecord
   # validates :attribute, phone: { possible: true, allow_blank: true, types: [:voip, :mobile], country_specifier: -> phone { phone.country.try(:upcase) } }
   enum role: {client: 0, admin: 1}
 
+  belongs_to :parent, class_name: 'User', optional: true, counter_cache: :children_members
+  has_many :children, class_name: 'User', foreign_key: 'parent_id'
   has_many :addresses
+
 end
