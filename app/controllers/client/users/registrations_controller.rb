0cf699c135f78dab1ff1f03f7e02a2747e8c6162
August Estidola <aie.estidola@gmail.com>
2023-11-13 15:21:22 +0800

    FEATURE 7 - Added parent_id to user

--- app/controllers/client/users/registrations_controller.rb
+++ app/controllers/client/users/registrations_controller.rb
@@ -1,6 +1,27 @@
 # frozen_string_literal: true
 
 class Client::Users::RegistrationsController < Devise::RegistrationsController
+  def new
+    super
+
+    if params[:promoter].present?
+      cookies[:promoter] = params[:promoter]
+    end
+  end
+
+  def create
+    super do |resource|
+      if (promoter_email = cookies.delete(:promoter)).present?
+        promoter = User.find_by(email: promoter_email)
+        resource.parent = promoter if promoter
+        resource.save
+      end
+    end
+  end
+
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :parent_id])
+  end
 
   def configure_sign_up_params
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
@@ -8,4 +29,5 @@ class Client::Users::RegistrationsController < Devise::RegistrationsController
   def configure_account_update_params
     devise_parameter_sanitizer.permit(:account_update, keys: [:phone, :username, :image])
   end
+
 end
