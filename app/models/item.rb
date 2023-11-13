3ae85d4a65847c98bb57a02362c13d7763183085
August Estidola <aie.estidola@gmail.com>
2023-11-13 17:21:39 +0800

    FEATURE 10 - Edited item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,12 @@
 class Item < ApplicationRecord
+  validates :image, presence: true
+  validates :name, presence: true
+  validates :quantity, presence: true
+  validates :minimum_tickets, presence: true
+  validates :state, presence: true
+  validates :batch_count, presence: true
+  validates :online_at, presence: true
+  validates :offline_at, presence: true
+  validates :start_at, presence: true
+  validates :status, presence: true
 end
