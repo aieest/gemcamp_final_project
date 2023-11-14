905cf40fbde7c1e3d1a4bbfecabe3ba253bf1ee0
August Estidola <aie.estidola@gmail.com>
2023-11-14 15:53:42 +0800

    FEATURE 10 (FOLLOW UP) - Created card partial for items

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,15 +1,18 @@
 class Item < ApplicationRecord
+
+  mount_uploader :image, ImageUploader
+
   default_scope { where(deleted_at: nil) }
   validates :image, presence: true
   validates :name, presence: true
   validates :quantity, presence: true
   validates :minimum_tickets, presence: true
-  validates :state, presence: true
+  # validates :state, presence: true
   validates :batch_count, presence: true
-  validates :online_at, presence: true
-  validates :offline_at, presence: true
-  validates :start_at, presence: true
-  validates :status, presence: true
+  # validates :online_at, presence: true
+  # validates :offline_at, presence: true
+  # validates :start_at, presence: true
+  # validates :status, presence: true
 
   def destroy
     update(deleted_at: Time.current)
@@ -17,4 +20,5 @@ class Item < ApplicationRecord
 
   has_many :item_category_ships
   has_many :categories, through: :item_category_ships
+
 end
